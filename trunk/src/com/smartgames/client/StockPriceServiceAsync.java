54ac5b7bc8c6590301ff966ab9e64b408eb41afe
provalentin@gmail.com <provalentin@gmail.com@2efc4aa6-6e35-fb24-d1a5-8f7bf400616f>
2012-05-30 13:40:03 +0000

    REVERTED to revision 60

--- trunk/src/com/smartgames/client/StockPriceServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.smartgames.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface StockPriceServiceAsync {
-  public void getPrices(String[] symbols, AsyncCallback<StockPrice[]> callback);
-}
