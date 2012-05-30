a78f25b2b5e516efe3ccba7039aad8099a999f63
provalentin <provalentin@gmail.com>
2012-05-30 13:40:03 +0000

    REVERTED to revision 60

--- src/com/smartgames/client/Durak_game.java
+++ src/com/smartgames/client/Durak_game.java
@@ -4,62 +4,28 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
-import com.google.gwt.event.dom.client.KeyPressEvent;
-import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.event.dom.client.MouseOutEvent;
 import com.google.gwt.event.dom.client.MouseOutHandler;
 import com.google.gwt.event.dom.client.MouseOverEvent;
 import com.google.gwt.event.dom.client.MouseOverHandler;
-import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.i18n.client.NumberFormat;
-import com.smartgames.client.StockPriceServiceAsync;
-import com.smartgames.client.StockService;
-import com.smartgames.client.StockServiceAsync;
-import com.smartgames.client.StockPrice;
-import com.smartgames.client.StockPriceService;
-import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FocusPanel;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TabBar;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Random;
 
 public class Durak_game implements EntryPoint {
 
-  private static final int REFRESH_INTERVAL = 5000; // ms
-  private VerticalPanel mainPanel = new VerticalPanel();
-  private FlexTable stocksFlexTable = new FlexTable();
-  private HorizontalPanel addPanel = new HorizontalPanel();
-  private TextBox newSymbolTextBox = new TextBox();
-  private Button addStockButton = new Button("Add");
-  private Label lastUpdatedLabel = new Label();
-  private HorizontalPanel horizontalPanel = new HorizontalPanel();
-  private HorizontalPanel rootPanel = new HorizontalPanel();
-  //private AbsolutePanel absolutePanel = new AbsolutePanel();
-  private VerticalPanel rightPanel = new VerticalPanel();
-  private ArrayList<String> stocks = new ArrayList<String>();
-  private StockPriceServiceAsync stockPriceSvc;
-  private final StockServiceAsync stockService = GWT.create(StockService.class);
-	
-	
-	
   private AbsolutePanel absolutePanel = new AbsolutePanel();
   private Button firstPlayerNextMoveButton = new Button("2");
   private Button secondPlayerNextMoveButton = new Button("2");
@@ -76,41 +42,35 @@ public class Durak_game implements EntryPoint {
    * Entry point method.
    */
   private LoginInfo loginInfo = null;
-  private HorizontalPanel loginPanel = new HorizontalPanel();
-  private Button loginButton = new Button("log in");
-  
-  private VerticalPanel tablesPanel = new VerticalPanel();
-  private Label tablesLabel = new Label("Tables");
-  private FlexTable tablesFlexTable = new FlexTable();
-  private Button createNewTableButton = new Button("Create new table");
-  private TextBox tableTextBox = new TextBox();
-  
-  private Label loginLabel = new Label("      your Google Account to access the Durak card game multiplayer mode  ");
-  private Anchor signInLink = new Anchor("Sign in to");
-  private Anchor signOutLink = new Anchor("Sign out");
+  private VerticalPanel loginPanel = new VerticalPanel();
+  private Label loginLabel = new Label("Please sign in to your Google Account to access the StockWatcher application.");
+  private Anchor signInLink = new Anchor("Sign In");
+  private Anchor signOutLink = new Anchor("Sign Out");
   private int currentPlayer = 1;
   private int mainKind;
   private boolean isGameOver;
 
   public void onModuleLoad() {
-	  prepareGame();  
-//	  RootPanel.get("loginItem").add(loginButton);
-	  loginButton.addClickHandler(loginButtonClickHandler); 
-	  loginPanel.add(loginButton);
-	  RootPanel.get("loginItem").add(loginPanel);
-	  loadStockWatcher();
-//	  addTablesPanel();
-	  
+	prepareGame();  
+	  // Check login status using login service.
+//    LoginServiceAsync loginService = GWT.create(LoginService.class);
+//    loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
+//      public void onFailure(Throwable error) {
+//      }
+//
+//      public void onSuccess(LoginInfo result) {
+//        loginInfo = result;
+//        if(loginInfo.isLoggedIn()) {
+//          //Window.alert("now we can start the game");
+//          prepareGame();
+//          //startGame();
+//        } else {
+//          loadLogin();
+//          Window.alert("Please log in before game starts");
+//        }
+//      }
+//    });
   }
-
-private void addTablesPanel() {
-	// TODO Auto-generated method stub
-	tablesPanel.add(tablesLabel);
-	tablesPanel.add(tablesFlexTable);
-	tablesPanel.add(tableTextBox);
-	tablesPanel.add(createNewTableButton);
-	RootPanel.get("loginItem").add(tablesPanel);
-}
 
 private void moveTableCardsToTrash() {
 	// TODO Auto-generated method stub
@@ -125,7 +85,7 @@ private void prepareGame() {
 	//absolutePanel = new AbsolutePanel();
 	RootPanel rootPanel = RootPanel.get("rootItem");
 	rootPanel.setSize("805", "660");
-	rootPanel.add(absolutePanel, 10, 30);
+	rootPanel.add(absolutePanel, 10, 10);
   	absolutePanel.setSize("800px", "650px");
   	
   	absolutePanel.add(firstPlayerNextMoveButton,600,260);
@@ -271,48 +231,13 @@ private void prepareGame() {
 			
 		}
 	};
-	
-private ClickHandler loginButtonClickHandler = new ClickHandler() {
-	@Override
-	public void onClick(ClickEvent event) {
-		// Check login status using login service.
-	    LoginServiceAsync loginService = GWT.create(LoginService.class);
-	    loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
-	      public void onFailure(Throwable error) {
-	      }
-
-	      public void onSuccess(LoginInfo result) {
-	        loginInfo = result;
-	        loadLogin();
-	        if(loginInfo.isLoggedIn()) {
-	        	//Window.alert("now we can start the game");
-	        	//prepareGame();
-	        	//startGame();
-	        	//Window.alert("already loged in");
-	        	signOutLink.setVisible(true);
-	        	signInLink.setVisible(false);
-	        	loginLabel.setVisible(false);
-	        } else {
-	        	loginLabel.setVisible(true);
-	        	signOutLink.setVisible(false);
-	        	signInLink.setVisible(true);
-	        	//Window.alert("Please log in before game starts");
-	        }
-	      }
-	    });
-	}
-};
   
 private void loadLogin() {
     // Assemble login panel.
     signInLink.setHref(loginInfo.getLoginUrl());
-    signOutLink.setHref(loginInfo.getLogoutUrl());
-    loginPanel.add(signInLink);
-    loginPanel.add(signOutLink);
     loginPanel.add(loginLabel);
-    signInLink.setVisible(false);
-    signOutLink.setVisible(false);
-//    RootPanel.get("loginItem").add(loginPanel);
+    loginPanel.add(signInLink);
+    RootPanel.get("loginItem").add(loginPanel);
 }
 
 private boolean playThisCard(Image image) {
@@ -457,265 +382,4 @@ private int calculateIndex(FocusPanel focusPanel) {
 	int topIndex = (focusPanel.getAbsoluteTop() - 427) / 40;
 	return leftIndex + 6 * topIndex;
 }
-
-private void loadStockWatcher() {
-	// Set up sign out hyperlink.
-    //signOutLink.setHref(loginInfo.getLogoutUrl());
-    
-	// Create table for stock data.
-    stocksFlexTable.setText(0, 0, "Symbol");
-    stocksFlexTable.setText(0, 1, "Price");
-    stocksFlexTable.setText(0, 2, "Change");
-    stocksFlexTable.setText(0, 3, "Remove");
-    stocksFlexTable.setText(0, 4, "History");
-
-    // Add styles to elements in the stock list table.
-    stocksFlexTable.setCellPadding(6);
-    stocksFlexTable.getRowFormatter().addStyleName(0, "watchListHeader");
-    stocksFlexTable.addStyleName("watchList");
-    stocksFlexTable.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
-    stocksFlexTable.getCellFormatter().addStyleName(0, 2, "watchListNumericColumn");
-    stocksFlexTable.getCellFormatter().addStyleName(0, 3, "watchListRemoveColumn");
-
-    loadStocks();
-
-    // Assemble Add Stock panel.
-    addPanel.add(newSymbolTextBox);
-    addPanel.add(addStockButton);
-    addPanel.addStyleName("addPanel");
-
-    // Assemble Main panel.
-    mainPanel.add(signOutLink);
-    mainPanel.add(stocksFlexTable);
-    stocksFlexTable.setWidth("385px");
-    
-    Button btnNewButton = new Button("New button");
-    mainPanel.add(btnNewButton);
-    mainPanel.add(addPanel);
-    mainPanel.add(lastUpdatedLabel);
-    
-    //rightPanel.add(horizontalPanel);
-    //rightPanel.add(absolutePanel);
-    
-    rootPanel.add(mainPanel);
-    rootPanel.add(rightPanel);
-   
-    // Associate the Main panel with the HTML host page.
-    RootPanel.get("rootItem").add(rootPanel);
-
-    // Move cursor focus to the input box.
-    newSymbolTextBox.setFocus(true);
-
-    // Setup timer to refresh list automatically.
-    Timer refreshTimer = new Timer() {
-      @Override
-      public void run() {
-        refreshWatchList();
-      }
-    };
-    refreshTimer.scheduleRepeating(REFRESH_INTERVAL);
-
-    // Listen for mouse events on the Add button.
-    addStockButton.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        addStock();
-      }
-    });
-
-    // Listen for keyboard events in the input box.
-    newSymbolTextBox.addKeyPressHandler(new KeyPressHandler() {
-      public void onKeyPress(KeyPressEvent event) {
-        if (event.getCharCode() == KeyCodes.KEY_ENTER) {
-          addStock();
-        }
-      }
-    });
-}
-
-  private void loadStocks() {
-    stockService.getStocks(new AsyncCallback<String[]>() {
-      public void onFailure(Throwable error) {
-      }
-      public void onSuccess(String[] symbols) {
-        displayStocks(symbols);
-      }
-    });
-  }
-
-  private void displayStocks(String[] symbols) {
-    for (String symbol : symbols) {
-      displayStock(symbol);
-    }
-  }
-
-  /**
-   * Add stock to FlexTable. Executed when the user clicks the addStockButton or
-   * presses enter in the newSymbolTextBox.
-   */
-  private void addStock() {
-	    final String symbol = newSymbolTextBox.getText().toUpperCase().trim();
-	    newSymbolTextBox.setFocus(true);
-
-	    // Stock code must be between 1 and 10 chars that are numbers, letters, or dots.
-	    if (!symbol.matches("^[0-9a-zA-Z\\.]{1,10}$")) {
-	      Window.alert("'" + symbol + "' is not a valid symbol.");
-	      newSymbolTextBox.selectAll();
-	      return;
-	    }
-
-	    newSymbolTextBox.setText("");
-
-	    // Don't add the stock if it's already in the table.
-	    if (stocks.contains(symbol))
-	      return;
-
-	    addStock(symbol);
-  }
-
-  private void addStock(final String symbol) {
-    stockService.addStock(symbol, new AsyncCallback<Void>() {
-      public void onFailure(Throwable error) {
-      }
-      public void onSuccess(Void ignore) {
-        displayStock(symbol);
-      }
-    });
-  }
-  private void displayStock(final String symbol) {
-	    // Add the stock to the table.
-	    int row = stocksFlexTable.getRowCount();
-	    stocks.add(symbol);
-	    stocksFlexTable.setText(row, 0, symbol);
-	    stocksFlexTable.setWidget(row, 2, new Label());
-	    stocksFlexTable.getCellFormatter().addStyleName(row, 1, "watchListNumericColumn");
-	    stocksFlexTable.getCellFormatter().addStyleName(row, 2, "watchListNumericColumn");
-	    stocksFlexTable.getCellFormatter().addStyleName(row, 3, "watchListRemoveColumn");
-
-	 // Add a button to remove this stock from the table.
-	    Button removeStock = new Button("x");
-	    removeStock.addStyleDependentName("remove");
-
-	    removeStock.addClickHandler(new ClickHandler(){
-	      public void onClick(ClickEvent event) {
-	        removeStock(symbol);
-	      }
-	    });
-	    stocksFlexTable.setWidget(row, 3, removeStock);
-
-	    // Get the stock price.
-	    refreshWatchList();
-
-	  }
-
-	  private void removeStock(final String symbol) {
-	    stockService.removeStock(symbol, new AsyncCallback<Void>() {
-	      public void onFailure(Throwable error) {
-	      }
-	      public void onSuccess(Void ignore) {
-	        undisplayStock(symbol);
-	      }
-	    });
-	  }
-
-	  private void undisplayStock(String symbol) {
-	    int removedIndex = stocks.indexOf(symbol);
-	    stocks.remove(removedIndex);
-	    stocksFlexTable.removeRow(removedIndex+1);
-	  }
-  protected void getStockHistory() {
-	// TODO Auto-generated method stub
-	
-}
-
-  /**
-   * Generate random stock prices.
-   */
-  private void refreshWatchList() {
-//    final double MAX_PRICE = 100.0; // $100.00
-//    final double MAX_PRICE_CHANGE = 0.02; // +/- 2%
-//
-//    StockPrice[] prices = new StockPrice[stocks.size()];
-//    for (int i = 0; i < stocks.size(); i++) {
-//      double price = Random.nextDouble() * MAX_PRICE;
-//      double change = price * MAX_PRICE_CHANGE
-//          * (Random.nextDouble() * 2.0 - 1.0);
-//
-//      prices[i] = new StockPrice(stocks.get(i), price, change);
-//    }
-//
-//    updateTable(prices);
-	// Initialize the service proxy.
-    if (stockPriceSvc == null) {
-      stockPriceSvc = GWT.create(StockPriceService.class);
-    }
-
-    // Set up the callback object.
-    AsyncCallback<StockPrice[]> callback = new AsyncCallback<StockPrice[]>() {
-      public void onFailure(Throwable caught) {
-        // TODO: Do something with errors.
-      }
-
-      public void onSuccess(StockPrice[] result) {
-        updateTable(result);
-      }
-    };
-
-    // Make the call to the stock price service.
-    stockPriceSvc.getPrices(stocks.toArray(new String[0]), callback);
-	  
-  }
-
-  /**
-   * Update the Price and Change fields all the rows in the stock table.
-   *
-   * @param prices Stock data for all rows.
-   */
-  private void updateTable(StockPrice[] prices) {
-    for (int i = 0; i < prices.length; i++) {
-      updateTable(prices[i]);
-    }
-
-    // Display timestamp showing last refresh.
-    lastUpdatedLabel.setText("Last update : "
-        + DateTimeFormat.getMediumDateTimeFormat().format(new Date()) + 
-        "\nPrices: " + Arrays.toString(prices));
-
-  }
-
-  /**
-   * Update a single row in the stock table.
-   *
-   * @param price Stock data for a single row.
-   */
-  private void updateTable(StockPrice price) {
-    // Make sure the stock is still in the stock table.
-    if (!stocks.contains(price.getSymbol())) {
-      return;
-    }
-
-    int row = stocks.indexOf(price.getSymbol()) + 1;
-
-    // Format the data in the Price and Change fields.
-    String priceText = NumberFormat.getFormat("#,##0.00").format(
-        price.getPrice());
-    NumberFormat changeFormat = NumberFormat.getFormat("+#,##0.00;-#,##0.00");
-    String changeText = changeFormat.format(price.getChange());
-    String changePercentText = changeFormat.format(price.getChangePercent());
-
-    // Populate the Price and Change fields with new data.
-    stocksFlexTable.setText(row, 1, priceText);
-    Label changeWidget = (Label)stocksFlexTable.getWidget(row, 2);
-    changeWidget.setText(changeText + " (" + changePercentText + "%)");
-
-    // Change the color of text in the Change field based on its value.
-    String changeStyleName = "noChange";
-    if (price.getChangePercent() < -0.1f) {
-      changeStyleName = "negativeChange";
-    }
-    else if (price.getChangePercent() > 0.1f) {
-      changeStyleName = "positiveChange";
-    }
-
-    changeWidget.setStyleName(changeStyleName);
-  }
 }
