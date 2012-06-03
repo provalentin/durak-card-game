b058e6de5daf9762ef127a260e8c0f7b2235e808
provalentin <provalentin@gmail.com>
2012-06-03 10:06:08 +0000

    saving allCards state as arrayList of cards

---
+++ src/com/smartgames/client/State.java
@@ -0,0 +1,13 @@
+package com.smartgames.client;
+
+public class State {
+	public static int inPack = 0;
+	public static int onTable = 1;
+	public static int played = 2;
+	public static int firstPlayerHand = 3;
+	public static int secondPlayerHand = 4;
+	public static int thirdPlayerHand = 5;
+	public static int fourthPlayerHand = 6;
+	public static int fifthPlayerHand = 7;
+	public static int sixthPlayerHand = 8;
+}
